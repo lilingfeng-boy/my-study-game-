fd5f621350c9c3bf92be2fbb6dd89a029da489a0
lilingfeng-boy <2238715039@qq.com>
2020-10-23 01:02:08 +0800

    第一次更新(只不过有错误)！

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+#include<unistd.h>
 #include<bits/stdc++.h>
 using namespace std;
+int random_number()
+{
+	int a = time(NULL);
+	int rn = (a / 1000 * a /3244) % 213351;
+	return rn;
+}
 int main()
 {
+	int number1, number2, math;
+	printf("***********3***********");
+	sleep(1000);
+	printf("\b\b\b\b\b\b\b\b\b\b\b\b2");
+	sleep(1000);
+	printf("\b\b\b\b\b\b\b\b\b\b\b\b1");
+	sleep(1000);
+	printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b                       ");
 	return 0;
 }
